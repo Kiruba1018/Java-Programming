9ce00a5d374bd6e0a5e0ce3db845e5a677898bca
Kirubagaran M <146163145+Kiruba1018@users.noreply.github.com>
2024-07-27 22:11:18 +0530

    Add files via upload

---
+++ Stack/Stack In Linkedlist/Stack.java
@@ -0,0 +1,11 @@
+
+public class Stack {
+
+     public static void main(String[] args) {
+        int x=10;
+        System.out.printf("%d ",(x++));
+        System.out.printf("%d ",(++x));
+        System.out.printf("%d ",(x--));
+        System.out.printf("%d ",(--x));
+     }
+}
