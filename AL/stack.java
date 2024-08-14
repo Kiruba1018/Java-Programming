8c1370e4db5a9999c8d1af682606c39ad05bd4b2
Kirubagaran M <146163145+Kiruba1018@users.noreply.github.com>
2024-08-14 18:14:49 +0530

    Add files via upload

---
+++ AL/stack.java
@@ -0,0 +1,11 @@
+import java.util.*;
+class stack {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        Stack <Integer> st = new Stack<>(); 
+        int ch = s.nextInt();
+        
+        
+    }
+    
+}
