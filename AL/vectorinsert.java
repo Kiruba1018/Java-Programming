1bec24d2599ddd1a269b07384d6e89ea52964895
Kirubagaran M <146163145+Kiruba1018@users.noreply.github.com>
2024-08-25 21:23:02 +0530

    Update Vectorinsert.java

--- AL/vectorinsert.java
+++ AL/vectorinsert.java
@@ -1,5 +1,5 @@
 import java.util.*;
-class vectorinsert {
+class Vectorinsert {
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
         Vector <Integer> v = new Vector<>();
