34a07fa2130e05b9884dc5bb2590ae5bdfdb682c
Kirubagaran M <146163145+Kiruba1018@users.noreply.github.com>
2024-08-29 22:26:42 +0530

    Update Ascii.java

--- String/Ascii.java
+++ String/Ascii.java
@@ -1,5 +1,5 @@
 import java.util.*;
-public class Ascii {
+public class ascii {
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
         String s1 = s.nextLine();
